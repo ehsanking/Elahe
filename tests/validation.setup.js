9d1e75f9bdb2745bd85a9bca7cfb51853cba62a4
Ehsan <1883051+ehsanking@users.noreply.github.com>
2026-02-11 13:48:07 +0330

    Add tests/validation.setup.js for setting up environment variables

---
+++ tests/validation.setup.js
@@ -0,0 +1,7 @@
+// validation.setup.js
+
+// Set up environment variables for tests
+
+process.env.DB_TEST_URL = 'your_test_db_url';
+process.env.API_KEY = 'your_api_key';
+process.env.OTHER_CONFIG = 'other_config_value';
